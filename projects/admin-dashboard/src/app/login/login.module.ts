a63d3d25fc3dadf640111ff0d67a5866be1c44d4
alexanderhodes <alexander.hodes@live.com>
2020-06-10 13:31:37 +0200

    improved login design

--- projects/admin-dashboard/src/app/login/login.module.ts
+++ projects/admin-dashboard/src/app/login/login.module.ts
@@ -3,16 +3,18 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {LoginComponent} from './components/public-api';
 import {LoginRoutingModule} from './login-routing.module';
+import {StyleLibModule} from '../../../../style-lib/src/lib/style-lib.module';
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    LoginRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        LoginRoutingModule,
+        StyleLibModule
+    ],
   exports: [
     LoginComponent
   ]
