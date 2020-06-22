23054bffd6d6651eac4205543b722e4c0f14742b
alexanderhodes <alexander.hodes@live.com>
2020-06-22 15:08:02 +0200

    updated environment

--- projects/admin-dashboard/src/environments/environment.prod.ts
+++ projects/admin-dashboard/src/environments/environment.prod.ts
@@ -1,17 +1,8 @@
 export const environment = {
   production: true,
-  firebaseConfig : {
-    apiKey: 'AIzaSyB_N6h4AgXYARY_Clmf7DZrmfsQ0yjobYI',
-    authDomain: 'colivery-app.firebaseapp.com',
-    databaseURL: 'https://colivery-app.firebaseio.com',
-    projectId: 'colivery-app',
-    storageBucket: 'colivery-app.appspot.com',
-    messagingSenderId: '645242671020',
-    appId: '1:645242671020:web:2764a5d651592e6f597409',
-    measurementId: 'G-XRNJT36EYT'
-  },
+  firebaseConfig : {},
   mapsUrl: 'https://maps.googleapis.com/maps/api/geocode/json',
-  mapsKey: 'AIzaSyDU2yAmHe4wAgIWhjWEyJcJBJqdjLKO8R0',
+  mapsKey: '',
   apiUrl: 'https://service-api-ng.nightly.staging.colivery.app/'
 };
 
