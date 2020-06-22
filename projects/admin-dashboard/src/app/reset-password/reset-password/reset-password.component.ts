0a9f8f4a63f317e1304c759cff37a5ecb518a46a
Bene <bene_b.99@web.de>
2020-06-22 12:48:50 +0200

    reset-password

--- projects/admin-dashboard/src/app/reset-password/reset-password/reset-password.component.ts
+++ projects/admin-dashboard/src/app/reset-password/reset-password/reset-password.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { AuthService } from '../../shared/public-api';
 
 @Component({
   selector: 'mbs-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.scss']
+  styleUrls: ['./reset-password.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  resetPasswordForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
+
+  emailError: boolean = false;
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onFormSubmit() {
+    if (this.resetPasswordForm.valid) {
+      // Form is valid and reset all errors
+      this.authService.sendMailResetPassword(this.resetPasswordForm.value.email)
+      .then(result => console.log(result))
+      .catch(error => console.log(error));
+      this.router.navigate(['login']).then();
+
+    } else {
+      if (this.resetPasswordForm.get('email').invalid) {
+        this.emailError = true;
+      }
+    }
+  }
 }
