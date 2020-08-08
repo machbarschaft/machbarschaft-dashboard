f793de37f55c42e9da22ac83734ed2e4cc42a9c7
alexanderhodes <alexander.hodes@live.com>
2020-08-08 12:27:56 +0200

    added comments for interceptor

--- projects/admin-dashboard/src/app/shared/services/request.interceptor.ts
+++ projects/admin-dashboard/src/app/shared/services/request.interceptor.ts
@@ -5,9 +5,14 @@ import {environment} from '../../../environments/environment';
 export class RequestInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // just update request for request that contain string v1 from our backend
     if (req.url.indexOf('v1') > -1) {
+      // get token from local storage
       const token = localStorage.getItem('token');
+      // get api url from environmewnt
       const host = environment.apiUrl;
+      // close request and add request headers for content type and authorization
+      // update url with host from environment
       const request = req.clone({
         headers: req.headers.set('Content-Type', 'application/json').set('Authorization', `Bearer ${token}`),
         url: `${host}${req.url}`
