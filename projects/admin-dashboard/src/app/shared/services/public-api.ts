e2ed81d0c6b7fed86d2d7a93f9fec0b7a392cac9
Aron Homberg <a.homberg@climatepartner.com>
2020-12-06 15:54:36 +0100

    fix: removed sodium and client-side password hashing for the passwords to match accross systems

--- projects/admin-dashboard/src/app/shared/services/public-api.ts
+++ projects/admin-dashboard/src/app/shared/services/public-api.ts
@@ -2,6 +2,5 @@ export * from './auth.service';
 export * from './authentication-guard.service';
 export * from './constants.translator.service';
 export * from './maps.service';
-export * from './sodium-crypto.service';
 export * from './backend/user.service';
 export * from './request.interceptor';
