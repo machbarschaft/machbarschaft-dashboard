96323eab9d75aebb4512b31eb8e2a3bd2e873930
alexanderhodes <alexander.hodes@live.com>
2020-12-30 08:51:49 +0100

    updated backend mock and improved error handling for user not found by adding to admin

--- projects/admin-dashboard-backend-mock/admin.js
+++ projects/admin-dashboard-backend-mock/admin.js
@@ -1,4 +1,6 @@
 
+const userDoesNotExist = 'test@machbarschaft.jetzt';
+
 module.exports = function(app) {
 
   /**
@@ -7,10 +9,12 @@ module.exports = function(app) {
    */
   app.put('/v1/admin/users/*', function (req, res) {
     const mail = req.params['0'];
-    if (mail && mail.indexOf('@') > 0) {
+    if (mail === userDoesNotExist) {
+      res.status(404).send(`user ${userDoesNotExist} does not exist`);
+    } else if (mail && mail.indexOf('@') > 0) {
       res.status(200).end();
     } else {
-      res.status(400).end();
+      res.status(404).end(`user ${mail} does not exist`);
     }
   });
 
